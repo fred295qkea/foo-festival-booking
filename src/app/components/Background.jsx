512066754cfd89233339adddb959f1a45350354e
”Frederik <fred295q@stud.kea.dk>
2024-01-16 21:41:37 +0100

    style hook-up

--- src/app/components/Background.jsx
+++ src/app/components/Background.jsx
@@ -1,268 +1,218 @@
 "use client";
-import * as THREE from 'three';
-import { useEffect, useRef } from "react"; 
-import styles from "./styles/Background.module.scss"
+import * as THREE from "three";
+import { useEffect, useRef } from "react";
+import styles from "./styles/Background.module.scss";
 
 function MyThree() {
   const refContainer = useRef(null);
   let group;
-const particlesData = [];
-let camera, scene, renderer;
-let positions, colors;
-let particles;
-let pointCloud;
-let particlePositions;
-let linesMesh;
-let targetX = 0;
-let targetY = 0;
+  const particlesData = [];
+  let camera, scene, renderer;
+  let positions, colors;
+  let particles;
+  let pointCloud;
+  let particlePositions;
+  let linesMesh;
+  let targetX = 0;
+  let targetY = 0;
 
-const maxParticleCount = 350;
-let particleCount = 350;
-const r = 450;
-const rLong = 800;
-const rHalf = r / 2;
+  const maxParticleCount = 350;
+  let particleCount = 350;
+  const r = 450;
+  const rLong = 800;
+  const rHalf = r / 2;
 
-const effectController = {
-  showLines: true,
-  minDistance: 130,
-  maxConnections: 5,
-};
-
-useEffect(() => {
-  init();
-  window.addEventListener("resize", onWindowResize);
-  window.addEventListener("mousemove", handleMouseMove);
-
-  refContainer.current.appendChild(renderer.domElement);
-
-
-  animate();
-
-  return () => {
-    window.removeEventListener("resize", onWindowResize);
-    window.removeEventListener("mousemove", handleMouseMove);
+  const effectController = {
+    showLines: true,
+    minDistance: 130,
+    maxConnections: 5,
   };
 
-},);
+  useEffect(() => {
+    init();
+    window.addEventListener("resize", onWindowResize);
+    window.addEventListener("mousemove", handleMouseMove);
 
-function init() {
-  camera = new THREE.PerspectiveCamera(
-    20,
-    window.innerWidth / window.innerHeight,
-    1,
-    4000
-  );
-  camera.position.z = 1350;
+    refContainer.current.appendChild(renderer.domElement);
 
-  scene = new THREE.Scene();
-  group = new THREE.Group();
-  scene.add(group);
+    animate();
 
-
-
-
-  const segments = maxParticleCount * maxParticleCount;
-
-  positions = new Float32Array(segments * 3);
-  colors = new Float32Array(segments * 3);
-
-  const pMaterial = new THREE.PointsMaterial({
-    color: 0xffffff,
-    blending: THREE.AdditiveBlending,
-    transparent: true,
-    sizeAttenuation: true,
+    return () => {
+      window.removeEventListener("resize", onWindowResize);
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
   });
 
-  particles = new THREE.BufferGeometry();
-  particlePositions = new Float32Array(maxParticleCount * 3);
+  function init() {
+    camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 1, 4000);
+    camera.position.z = 1350;
 
-  for (let i = 0; i < maxParticleCount; i++) {
-    const x = Math.random() * r - r / 2;
-    const y = Math.random() * rLong - rLong / 2;
-    const z = Math.random() *  r / 2;
+    scene = new THREE.Scene();
+    group = new THREE.Group();
+    scene.add(group);
 
-    particlePositions[i * 3] = x;
-    particlePositions[i * 3 + 1] = y;
-    particlePositions[i * 3 + 2] = z;
+    const segments = maxParticleCount * maxParticleCount;
 
-    // add it to the geometry
-    particlesData.push({
-      velocity: new THREE.Vector3(
-        -0.5 + Math.random() * 0.5,
-        -0.5 + Math.random() * 0.5,
-        -0.5 + Math.random() * 0.5 
-      ),
-      numConnections: 0,
+    positions = new Float32Array(segments * 3);
+    colors = new Float32Array(segments * 3);
+
+    const pMaterial = new THREE.PointsMaterial({
+      color: 0xffffff,
+      blending: THREE.AdditiveBlending,
+      transparent: true,
+      sizeAttenuation: true,
     });
+
+    particles = new THREE.BufferGeometry();
+    particlePositions = new Float32Array(maxParticleCount * 3);
+
+    for (let i = 0; i < maxParticleCount; i++) {
+      const x = Math.random() * r - r / 2;
+      const y = Math.random() * rLong - rLong / 2;
+      const z = (Math.random() * r) / 2;
+
+      particlePositions[i * 3] = x;
+      particlePositions[i * 3 + 1] = y;
+      particlePositions[i * 3 + 2] = z;
+
+      // add it to the geometry
+      particlesData.push({
+        velocity: new THREE.Vector3(-0.5 + Math.random() * 0.5, -0.5 + Math.random() * 0.5, -0.5 + Math.random() * 0.5),
+        numConnections: 0,
+      });
+    }
+
+    particles.setDrawRange(0, particleCount);
+    particles.setAttribute("position", new THREE.BufferAttribute(particlePositions, 3).setUsage(THREE.DynamicDrawUsage));
+
+    // create the particle system
+    pointCloud = new THREE.Points(particles, pMaterial);
+    group.add(pointCloud);
+
+    const geometry = new THREE.BufferGeometry();
+
+    geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3).setUsage(THREE.DynamicDrawUsage));
+    geometry.setAttribute("color", new THREE.BufferAttribute(colors, 3).setUsage(THREE.DynamicDrawUsage));
+
+    geometry.computeBoundingSphere();
+
+    geometry.setDrawRange(0, 0);
+
+    const material = new THREE.LineBasicMaterial({
+      vertexColors: true,
+      blending: THREE.AdditiveBlending,
+      transparent: true,
+      opacity: 0.5,
+      color: "white",
+    });
+
+    linesMesh = new THREE.LineSegments(geometry, material);
+    group.add(linesMesh);
+
+    //
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    //
+
+    window.addEventListener("resize", onWindowResize);
   }
 
-  particles.setDrawRange(0, particleCount);
-  particles.setAttribute(
-    "position",
-    new THREE.BufferAttribute(particlePositions, 3).setUsage(
-      THREE.DynamicDrawUsage
-    )
-  );
+  function animate() {
+    let vertexpos = 0;
+    let colorpos = 0;
+    let numConnected = 0;
 
-  // create the particle system
-  pointCloud = new THREE.Points(particles, pMaterial);
-  group.add(pointCloud);
+    for (let i = 0; i < particleCount; i++) particlesData[i].numConnections = 0;
 
-  const geometry = new THREE.BufferGeometry();
+    for (let i = 0; i < particleCount; i++) {
+      // get the particle
+      const particleData = particlesData[i];
 
-  geometry.setAttribute(
-    "position",
-    new THREE.BufferAttribute(positions, 3).setUsage(THREE.DynamicDrawUsage)
-  );
-  geometry.setAttribute(
-    "color",
-    new THREE.BufferAttribute(colors, 3).setUsage(THREE.DynamicDrawUsage)
-  );
+      particlePositions[i * 3] += particleData.velocity.x;
+      particlePositions[i * 3 + 1] += particleData.velocity.y;
+      particlePositions[i * 3 + 2] += particleData.velocity.z;
 
-  geometry.computeBoundingSphere();
+      if (particlePositions[i * 3 + 1] < -rHalf || particlePositions[i * 3 + 1] > rHalf) particleData.velocity.y = -particleData.velocity.y;
 
-  geometry.setDrawRange(0, 0);
+      if (particlePositions[i * 3] < -rHalf || particlePositions[i * 3] > rHalf) particleData.velocity.x = -particleData.velocity.x;
 
-  const material = new THREE.LineBasicMaterial({
-    vertexColors: true,
-    blending: THREE.AdditiveBlending,
-    transparent: true,
-    opacity: 0.5,
-    color: 0x0abdc6,
-  });
+      if (particlePositions[i * 3 + 2] < -rHalf || particlePositions[i * 3 + 2] > rHalf) particleData.velocity.z = -particleData.velocity.z;
 
-  linesMesh = new THREE.LineSegments(geometry, material);
-  group.add(linesMesh);
+      if (effectController.limitConnections && particleData.numConnections >= effectController.maxConnections) continue;
 
-  //
+      // Check collision
+      for (let j = i + 1; j < particleCount; j++) {
+        const particleDataB = particlesData[j];
+        if (effectController.limitConnections && particleDataB.numConnections >= effectController.maxConnections) continue;
 
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
+        const dx = particlePositions[i * 3] - particlePositions[j * 3];
+        const dy = particlePositions[i * 3 + 1] - particlePositions[j * 3 + 1];
+        const dz = particlePositions[i * 3 + 2] - particlePositions[j * 3 + 2];
+        const dist = Math.sqrt(dx * dx + dy * dy + dz * dz);
 
-  //
+        if (dist < effectController.minDistance) {
+          particleData.numConnections++;
+          particleDataB.numConnections++;
 
+          const alpha = 1.0 - dist / effectController.minDistance;
 
-  window.addEventListener("resize", onWindowResize);
-}
+          positions[vertexpos++] = particlePositions[i * 3];
+          positions[vertexpos++] = particlePositions[i * 3 + 1];
+          positions[vertexpos++] = particlePositions[i * 3 + 2];
 
+          positions[vertexpos++] = particlePositions[j * 3];
+          positions[vertexpos++] = particlePositions[j * 3 + 1];
+          positions[vertexpos++] = particlePositions[j * 3 + 2];
 
-function animate() {
-  let vertexpos = 0;
-  let colorpos = 0;
-  let numConnected = 0;
+          colors[colorpos++] = alpha;
+          colors[colorpos++] = alpha;
+          colors[colorpos++] = alpha;
 
-  for (let i = 0; i < particleCount; i++) particlesData[i].numConnections = 0;
+          colors[colorpos++] = alpha;
+          colors[colorpos++] = alpha;
+          colors[colorpos++] = alpha;
 
-  for (let i = 0; i < particleCount; i++) {
-    // get the particle
-    const particleData = particlesData[i];
-
-    particlePositions[i * 3 ] += particleData.velocity.x;
-    particlePositions[i * 3 + 1] += particleData.velocity.y;
-    particlePositions[i * 3 + 2] += particleData.velocity.z;
-
-    if (
-      particlePositions[i * 3 + 1] < -rHalf ||
-      particlePositions[i * 3 + 1] > rHalf
-    )
-      particleData.velocity.y = -particleData.velocity.y;
-
-    if (particlePositions[i * 3] < -rHalf || particlePositions[i * 3] > rHalf)
-      particleData.velocity.x = -particleData.velocity.x;
-
-    if (
-      particlePositions[i * 3 + 2] < -rHalf ||
-      particlePositions[i * 3 + 2] > rHalf
-    )
-      particleData.velocity.z = -particleData.velocity.z;
-
-    if (
-      effectController.limitConnections &&
-      particleData.numConnections >= effectController.maxConnections
-    )
-      continue;
-
-    // Check collision
-    for (let j = i + 1; j < particleCount; j++) {
-      const particleDataB = particlesData[j];
-      if (
-        effectController.limitConnections &&
-        particleDataB.numConnections >= effectController.maxConnections
-      )
-        continue;
-
-      const dx = particlePositions[i * 3] - particlePositions[j * 3];
-      const dy = particlePositions[i * 3 + 1] - particlePositions[j * 3 + 1];
-      const dz = particlePositions[i * 3 + 2] - particlePositions[j * 3 + 2];
-      const dist = Math.sqrt(dx * dx + dy * dy + dz * dz);
-
-      if (dist < effectController.minDistance) {
-        particleData.numConnections++;
-        particleDataB.numConnections++;
-
-        const alpha = 1.0 - dist / effectController.minDistance;
-
-        positions[vertexpos++] = particlePositions[i * 3];
-        positions[vertexpos++] = particlePositions[i * 3 + 1];
-        positions[vertexpos++] = particlePositions[i * 3 + 2];
-
-        positions[vertexpos++] = particlePositions[j * 3];
-        positions[vertexpos++] = particlePositions[j * 3 + 1];
-        positions[vertexpos++] = particlePositions[j * 3 + 2];
-
-        colors[colorpos++] = alpha;
-        colors[colorpos++] = alpha;
-        colors[colorpos++] = alpha;
-
-        colors[colorpos++] = alpha;
-        colors[colorpos++] = alpha;
-        colors[colorpos++] = alpha;
-
-        numConnected++;
+          numConnected++;
+        }
       }
     }
+
+    linesMesh.geometry.setDrawRange(0, numConnected * 2);
+    linesMesh.geometry.attributes.position.needsUpdate = true;
+    linesMesh.geometry.attributes.color.needsUpdate = true;
+
+    pointCloud.geometry.attributes.position.needsUpdate = true;
+
+    requestAnimationFrame(animate);
+
+    render();
   }
 
-  linesMesh.geometry.setDrawRange(0, numConnected * 2);
-  linesMesh.geometry.attributes.position.needsUpdate = true;
-  linesMesh.geometry.attributes.color.needsUpdate = true;
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
 
-  pointCloud.geometry.attributes.position.needsUpdate = true;
+  function handleMouseMove(event) {
+    targetX = event.clientX * 0.001;
+    targetY = event.clientY * 0.001;
+  }
 
-  requestAnimationFrame(animate);
+  function render() {
+    // Use lerp to smoothly transition from the current rotation to the target rotation
+    group.rotation.y += (targetX - group.rotation.y) * 0.02;
+    group.rotation.x += (targetY - group.rotation.x) * 0.02;
 
-  render();
-  
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.render(scene, camera);
+  }
+
+  return <div ref={refContainer} className={styles.anim}></div>;
 }
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function handleMouseMove(event) {
-  targetX = event.clientX * 0.0010;
-  targetY = event.clientY * 0.0010;
-}
-
-function render() {
-  // Use lerp to smoothly transition from the current rotation to the target rotation
-  group.rotation.y += (targetX - group.rotation.y) * 0.020;
-  group.rotation.x += (targetY - group.rotation.x) * 0.020;
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
-  renderer.render(scene, camera);
-}
-
-  return (
-    <div ref={refContainer} className={styles.anim}></div>
-
-  );
-}
-
-
 
 export default MyThree;
--- src/app/components/Menu.jsx
+++ src/app/components/Menu.jsx
@@ -1,38 +1,38 @@
 "use client";
 import styles from "./styles/Menu.module.scss";
-import { useState} from "react";
+import { useState } from "react";
 import Link from "next/link";
-import { Turn as Hamburger } from 'hamburger-react'
+import { Turn as Hamburger } from "hamburger-react";
 import Image from "next/image";
 
 export default function NavMenu() {
-    const [isOpen, setisOpen] = useState(false);
+  const [isOpen, setisOpen] = useState(false);
 
+  const toggleOpen = () => {
+    setisOpen(!isOpen);
+  };
 
-    const toggleOpen = () => {
-        setisOpen(!isOpen);
-    }
-
-
-
-    return (
-        <nav className={styles.nav}>
-            <Link href="/">
-                <Image
-                src="/logo_white.png"
-                height={60}
-                width={60}
-                alt="logo"
-                />
-                </Link>
-            <button className={styles.toggle_button} onClick={toggleOpen}><Hamburger color="white" duration={0.3} easing="ease-in" size={25}/></button>
-            <div className={isOpen ? styles.active : styles.menu}>
-                <ul>
-                    <Link href='/'><li>HOME</li></Link>
-                    <Link href='/artists'><li>PROGRAM</li></Link>
-                    <Link href='/paystep'><li>TICKETS</li></Link>
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className={styles.nav}>
+      <Link href="/">
+        <Image src="/logo_white.png" height={50} width={50} alt="logo" />
+      </Link>
+      <button className={styles.toggle_button} onClick={toggleOpen}>
+        <Hamburger color="white" duration={0.3} easing="ease-in" size={25} />
+      </button>
+      <div className={isOpen ? styles.active : styles.menu}>
+        <ul>
+          <Link href="/">
+            <li>HOME</li>
+          </Link>
+          <Link href="/artists">
+            <li>PROGRAM</li>
+          </Link>
+          <Link href="/paystep">
+            <li>TICKETS</li>
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
 }
