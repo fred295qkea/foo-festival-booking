a0702cbf97db7910a26c6b84cb3686626a760842
”Frederik <fred295q@stud.kea.dk>
2024-01-17 11:59:15 +0100

    border r fix

--- src/app/components/OptionToTicket.jsx
+++ src/app/components/OptionToTicket.jsx
@@ -1,133 +1,114 @@
-import { useState, useEffect } from 'react';
-import styles from './styles/OptionToTicket.module.scss'
+import { useState, useEffect } from "react";
+import styles from "./styles/OptionToTicket.module.scss";
 import Option from "./Option";
 
-function OptionToTicket(props){
-   
-    const [errorDescription, setErrorDescription] = useState("")
-    const [data, setData] = useState(null);
-   
-    let maxTent = props.twoPerTent+props.threePerTent;
-const checkErrorFun = (event)=>{
+function OptionToTicket(props) {
+  const [errorDescription, setErrorDescription] = useState("");
+  const [data, setData] = useState(null);
+
+  let maxTent = props.twoPerTent + props.threePerTent;
+  const checkErrorFun = (event) => {
     event.preventDefault();
-    let camp = document.querySelector('input[type=radio][name=camp]:checked');
-    
-    if(maxTent>props.tickets){
-       setErrorDescription("You can't buy more tents than tickets")
+    let camp = document.querySelector("input[type=radio][name=camp]:checked");
+
+    if (maxTent > props.tickets) {
+      setErrorDescription("You can't buy more tents than tickets");
     } else if (!camp) {
-        setErrorDescription("You need to choose a camp")
-    }else if(props.tickets > camp.getAttribute('data-available')){
-        setErrorDescription("spot is shit")
-    }else{
-        props.setCamp(camp.value);
+      setErrorDescription("You need to choose a camp");
+    } else if (props.tickets > camp.getAttribute("data-available")) {
+      setErrorDescription("Spot unaviable");
+    } else {
+      props.setCamp(camp.value);
 
-       // const reserveSpot = async () => {
-        async function reserveSpot(){
-            try {
-              const response = await fetch(`${process.env.NEXT_PUBLIC_URL}reserve-spot`, {
-                method: 'PUT',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  area: camp.value,
-                  amount: props.tickets,
-                }),
-              });
-      
-              if (!response.ok) {
-                throw new Error('Network response was not ok');
-              }
-      
-              const result = await response.json();
-              console.log('Data blev opdateret:', result.id);
-              if(result.message == "Reserved"){
-               console.log(result);
-                props.setPostId(result.id);
-                props.setCurrentStep(2)
-              }
-            } catch (error) {
-              console.error('Fejl ved PUT-anmodning:', error);
-              setErrorDescription("error try again later");
-            }
-          };
-          reserveSpot();
-        
-    }
-}
-
-    
-  
-    useEffect(() => {
-      // Funktion for at hente data
-      const fetchData = async () => {
+      // const reserveSpot = async () => {
+      async function reserveSpot() {
         try {
-          const response = await fetch(process.env.NEXT_PUBLIC_URL + 'available-spots', {
-            method: 'GET',
+          const response = await fetch(`${process.env.NEXT_PUBLIC_URL}reserve-spot`, {
+            method: "PUT",
             headers: {
-              'Content-Type': 'application/json'
+              "Content-Type": "application/json",
             },
+            body: JSON.stringify({
+              area: camp.value,
+              amount: props.tickets,
+            }),
           });
-  
+
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+
           const result = await response.json();
-  
-          // Opdater state med den hentede data
-          setData(result);
-  
-          
+          console.log("Data blev opdateret:", result.id);
+          if (result.message == "Reserved") {
+            console.log(result);
+            props.setPostId(result.id);
+            props.setCurrentStep(2);
+          }
         } catch (error) {
-          console.error('Fejl ved hentning af data:', error);
+          console.error("Fejl ved PUT-anmodning:", error);
+          setErrorDescription("error try again later");
         }
-      };
-  
-      // Kald fetchData-funktionen når komponenten er blevet monteret
-      fetchData();
-    }, []); // Tomt array som dependency for at sikre, at useEffect kun kører ved mount
-  
-    
+      }
+      reserveSpot();
+    }
+  };
 
-return(
-     
+  useEffect(() => {
+    // Funktion for at hente data
+    const fetchData = async () => {
+      try {
+        const response = await fetch(process.env.NEXT_PUBLIC_URL + "available-spots", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const result = await response.json();
+
+        // Opdater state med den hentede data
+        setData(result);
+      } catch (error) {
+        console.error("Fejl ved hentning af data:", error);
+      }
+    };
+
+    // Kald fetchData-funktionen når komponenten er blevet monteret
+    fetchData();
+  }, []); // Tomt array som dependency for at sikre, at useEffect kun kører ved mount
+
+  return (
     <div className={styles.container}>
-        <form className={styles.form} onSubmit={checkErrorFun}>
-            <div>
-                    <Option name={props.ticketType} price={props.ticketPris} tickets={10}  setNumber={props.setTickets} number={props.tickets}/>    
-            </div>
-            
+      <form className={styles.form} onSubmit={checkErrorFun}>
+        <div>
+          <Option name={props.ticketType} price={props.ticketPris} tickets={10} setNumber={props.setTickets} number={props.tickets} />
+        </div>
 
-            <div className={styles.tent_container}>
-                    <p className={styles.headline}>Choose Tent(s)</p>
-                    <Option name={"2 Person tent"} price={249} maxTent={maxTent} tickets={props.tickets} setNumber={props.setTwoPerTent}  number={props.twoPerTent}/>
-                    <Option name={"3 Person tent"} price={349} maxTent={maxTent} tickets={props.tickets}  setNumber={props.setThreeoPerTent}  number={props.threePerTent}/>
+        <div className={styles.tent_container}>
+          <p className={styles.headline}>Choose Tent(s)</p>
+          <Option name={"2 Person tent"} price={249} maxTent={maxTent} tickets={props.tickets} setNumber={props.setTwoPerTent} number={props.twoPerTent} />
+          <Option name={"3 Person tent"} price={349} maxTent={maxTent} tickets={props.tickets} setNumber={props.setThreeoPerTent} number={props.threePerTent} />
+        </div>
 
-            </div>
-            
-            
-            <div className={styles.campContainer}>
-                
-                    <p className={styles.headline}>Choose a Camp</p>
-                    {
-                        data?
-                        data.map((single, any)=>(
-                            <div data-number={any} key={single.area}>
-                                <label for={single.area} >{single.area} <span>{" - available spots: "+single.available}</span> </label>
-                                <input className={styles.radioButton} data-available={single.available}  type="radio" id={single.area} name="camp" value={single.area}/>
-                            </div>
-                        )):"Loading camps"
-                    }
-
-                    
-
-            </div>
-            <p className={styles.error}>{errorDescription}</p>
-            <button className={styles.valg}>Gem valg</button>
-        </form>
+        <div className={styles.campContainer}>
+          <p className={styles.headline}>Choose a Camp</p>
+          {data
+            ? data.map((single, any) => (
+                <div data-number={any} key={single.area}>
+                  <label for={single.area}>
+                    {single.area} <span>{" - available spots: " + single.available}</span>{" "}
+                  </label>
+                  <input className={styles.radioButton} data-available={single.available} type="radio" id={single.area} name="camp" value={single.area} />
+                </div>
+              ))
+            : "Loading camps"}
+        </div>
+        <p className={styles.error}>{errorDescription}</p>
+        <button className={styles.valg}>Gem valg</button>
+      </form>
     </div>
-    
-    
-
-)
-
+  );
 }
 
 export default OptionToTicket;
--- src/app/components/PaymentOptions.jsx
+++ src/app/components/PaymentOptions.jsx
@@ -1,230 +1,198 @@
+import styles from "./styles/PaymentOptions.module.scss";
+import React from "react";
+import Creditcard from "../components/CreditCard";
+import { useState } from "react";
+import Countdown from "react-countdown";
 
-import styles from './styles/PaymentOptions.module.scss'
-import React from 'react';
-import Creditcard from '../components/CreditCard';
-import { useState } from 'react';
-import Countdown from 'react-countdown';
+function PaymentOptions(props) {
+  const [isCardAdded, setIsCardAdded] = useState(false);
+  const [showButton, setShowButton] = useState(false);
 
+  // laver en array ud af antallet af billetter
+  const ticketNames = Array(props.tickets).fill("");
 
-    function PaymentOptions(props){
+  //the countdownfunction
+  const renderer = ({ minutes, seconds, completed }) => {
+    if (completed) {
+      props.setCurrentStep(0);
+    }
 
-      const [isCardAdded, setIsCardAdded] = useState(false);
-      const [showButton, setShowButton] = useState(false);
+    return (
+      <span>
+        {minutes}:{seconds}
+      </span>
+    );
+  };
 
-      // laver en array ud af antallet af billetter
-      const ticketNames = Array(props.tickets).fill('');
+  // når onSubmit aktiveres
+  function sendPostRequest(event) {
+    event.preventDefault();
 
-        //the countdownfunction 
-        const renderer = ({ minutes, seconds, completed }) => {
-            if (completed) {
-                props.setCurrentStep(0)
-              }
-              
-              return <span>{minutes}:{seconds}</span>;
-          };
+    //tomt object er det som skal sendes med så vi har navnene på billeter
+    const updatedTicketData = {};
 
+    //
+    ticketNames.forEach((e, i) => {
+      console.log(event.target.elements[`ticket${i + 1}`].value);
+      updatedTicketData[`billet-${i + 1}`] = event.target.elements[`ticket${i + 1}`].value;
+    });
 
+    console.log(updatedTicketData);
+    sendInfo(event, updatedTicketData);
+  }
 
+  function payticket() {
+    setIsCardAdded(true);
+    setShowButton(true);
+  }
 
-          // når onSubmit aktiveres 
-           function sendPostRequest(event){
-            event.preventDefault();
+  async function sendInfo(event, updatedTicketData) {
+    const response = await fetch(`https://bzhijbeuftyjosmsqsba.supabase.co/rest/v1/foo-table`, {
+      method: "POST",
+      headers: {
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6aGlqYmV1ZnR5am9zbXNxc2JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI3NDUwMTUsImV4cCI6MjAxODMyMTAxNX0.0RS_C54MUy2Wc2uyLOVWqbsPYljdCI1xF8xEihsWfJg",
+        "Content-Type": "application/json",
+        Prefer: "return=minimal",
+      },
+      body: JSON.stringify({
+        first_name: event.target.elements.fname.value,
+        last_name: event.target.elements.lname.value,
+        address: event.target.elements.adress.value,
+        country: event.target.elements.country.value,
+        city: event.target.elements.city.value,
+        email: event.target.elements.email.value,
+        phone: event.target.elements.tel.value ? event.target.elements.tel.value : 0,
+        ticketsName: updatedTicketData,
+      }),
+    });
+    console.log(response);
 
-            //tomt object er det som skal sendes med så vi har navnene på billeter 
-            const updatedTicketData = {};
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    } else {
+      reserveSpot();
+    }
+  }
 
-           //   
-            ticketNames.forEach((e,i)=>{
-              console.log(event.target.elements[`ticket${i + 1}`].value);
-              updatedTicketData[`billet-${i + 1}`] = event.target.elements[`ticket${i + 1}`].value;
-            })
-            
-            console.log(updatedTicketData);
-            sendInfo(event,updatedTicketData);
-            
-            
-            
-           }
+  async function reserveSpot() {
+    console.log("kør");
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_URL}fullfill-reservation`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: props.postId,
+        }),
+      });
 
-           function payticket(){
-            setIsCardAdded(true)
-            setShowButton(true);
-           }
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
 
-            async function sendInfo(event,updatedTicketData) {
-                
-                
-                  const response = await fetch(`https://bzhijbeuftyjosmsqsba.supabase.co/rest/v1/foo-table`, {
-                    method: 'POST',
-                    headers: {
-                      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6aGlqYmV1ZnR5am9zbXNxc2JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI3NDUwMTUsImV4cCI6MjAxODMyMTAxNX0.0RS_C54MUy2Wc2uyLOVWqbsPYljdCI1xF8xEihsWfJg",
-                      'Content-Type': 'application/json',
-                      "Prefer": "return=minimal",
-                    },
-                    body: JSON.stringify({
-                      first_name: event.target.elements.fname.value,
-                       last_name: event.target.elements.lname.value,
-                       address: event.target.elements.adress.value,
-                       country: event.target.elements.country.value,
-                       city: event.target.elements.city.value,
-                       email: event.target.elements.email.value,
-                       phone: event.target.elements.tel.value?event.target.elements.tel.value:0,
-                      ticketsName: updatedTicketData,
-                    }),
-                  });
-                  console.log(response);
-               
-              
-                  if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                  }else{
-                    reserveSpot();
-                  }
-              
-              };
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error("Fejl ved POST-anmodning:", error);
+    }
+  }
 
+  let subtotal = props.tickets * props.ticketPris + props.twoPerTent * 249 + props.threePerTent * 349;
+  return (
+    <>
+      <form className={styles.pay_info_form} onSubmit={sendPostRequest}>
+        <div className={`${styles.paysection} ${isCardAdded ? styles.show : ""}`}>
+          <div>
+            <Creditcard />
+            <button
+              className={styles.cardbutton}
+              type="button"
+              onClick={() => {
+                setIsCardAdded(false);
+              }}
+            >
+              {" "}
+              submit
+            </button>
+          </div>
+        </div>
 
-              
-              async function reserveSpot(){
-                console.log("kør");
-                try {
-                  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}fullfill-reservation`, {
-                    method: 'POST',
-                    headers: {
-                      'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        id: props.postId,
-                    }),
-                  });
-          
-                  if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                  }
-
-                const result = await response.json();
-                console.log(result);
-          
-                  
-                } catch (error) {
-                  console.error('Fejl ved POST-anmodning:', error);
-                  
-                }
-              };
-            
-
-              
-          
-
-      
-     let subtotal = props.tickets*props.ticketPris+props.twoPerTent*249+props.threePerTent*349;
-    return(
-        <>
-        
-            
-        
-        <form className={styles.pay_info_form} onSubmit={sendPostRequest}>
-
-<div className={`${styles.paysection} ${isCardAdded ? styles.show : ''}`}>
-
-<div >
-<Creditcard/>
-<button className={styles.cardbutton} type="button" onClick={()=>{setIsCardAdded(false)}} > submit</button>
-</div>
-</div>
-          
         <div className={styles.container}>
-        <div className={styles.under_container}>
-        <p>Contact info</p>
-        <div className={styles.inputsection}>
-        
-            <div>
-              
-                <input type="text" id="fname" name="fname" placeholder='Firstname' />
-                <input type="text" id="lname" name="lname" placeholder='Lastname' />
-            </div>
-            <div>
-                <input type="text" id="adress" name="adress" placeholder='Adress' />
-            </div>
-            <div>
-                <input type="text" id="Country" name="country" placeholder='Country' />
-                <input type="text" id="city" name="city" placeholder='City' />
-            </div>
-            <div>
-                <input type="email" id="email" name="email" placeholder='Email' />
-            </div>
-            <div>
-                <input type="tel" id="tel" name="tel" placeholder='Phone number' />
-            </div>
-            <br />
-            <hr />
-
-            {/* laver ligså mange input som der er billetter  */}
-            {ticketNames.map((ticketName, index) => (
-              <div key={index}>
-                <label>{`billet ${index + 1}:`}</label>
-                <input name={`ticket${index + 1}`}
-                  type="text"
-                placeholder='Name'
-                
-                />
-
+          <div className={styles.under_container}>
+            <p>Contact info</p>
+            <div className={styles.inputsection}>
+              <div>
+                <input type="text" id="fname" name="fname" placeholder="Firstname" />
+                <input type="text" id="lname" name="lname" placeholder="Lastname" />
               </div>
-            ))}
+              <div>
+                <input type="text" id="adress" name="adress" placeholder="Adress" />
+              </div>
+              <div>
+                <input type="text" id="Country" name="country" placeholder="Country" />
+                <input type="text" id="city" name="city" placeholder="City" />
+              </div>
+              <div>
+                <input type="email" id="email" name="email" placeholder="Email" />
+              </div>
+              <div>
+                <input type="tel" id="tel" name="tel" placeholder="Phone number" />
+              </div>
+              <br />
+              <hr />
 
-              
-           
+              {/* laver ligså mange input som der er billetter  */}
+              {ticketNames.map((ticketName, index) => (
+                <div key={index}>
+                  <label>{`billet ${index + 1}:`}</label>
+                  <input name={`ticket${index + 1}`} type="text" placeholder="Name" />
+                </div>
+              ))}
             </div>
-            
+          </div>
 
-        </div>
-    
-        <div className={styles.prisContainer}>
+          <div className={styles.prisContainer}>
             <p>Card summery</p>
-                <div className={styles.count}> <p>Time to order:</p>
-                 <Countdown date={Date.now() + 900000 } renderer={renderer} >
-                    
-                </Countdown> 
-                </div>
-                <div className={styles.prisElement}>
-                    <div>
-                        <p>Number of tickets:</p>
-                        <p><span>{props.tickets}x</span> Foo fest</p>
-                        <p>{props.ticketType}</p>
+            <div className={styles.count}>
+              {" "}
+              <p>Time to order:</p>
+              <Countdown date={Date.now() + 900000} renderer={renderer}></Countdown>
+            </div>
+            <div className={styles.prisElement}>
+              <div>
+                <p>Number of tickets:</p>
+                <p>
+                  <span>{props.tickets}x</span> Foo fest
+                </p>
+                <p>{props.ticketType}</p>
+              </div>
+              <div>
+                <p>2-person tent: {props.twoPerTent}</p>
+                <p>3-person tent: {props.threePerTent}</p>
+              </div>
+              <div>
+                <p>Subtotal: {subtotal}kr.</p>
+              </div>
+              <div>
+                <p>Booking fee 99kr.</p>
+              </div>
 
-                    </div>
-                    <div>
-                        <p>2-person tent: {props.twoPerTent}</p>
-                        <p>3-person tent: {props.threePerTent}</p>
+              <p className={styles.Total}>Total: {subtotal + 99}</p>
 
-                    </div>
-                    <div >
-                        <p>Subtotal: {subtotal}kr.</p>
-                    </div>
-                    <div>
-                        <p>Booking fee 99kr.</p>
-                    </div>
-
-                    <p className={styles.Total}>Total: {subtotal+99}</p>
-
-                   
-            
-                  
-                 
-            
-            <button className={styles.creditcard_button} onClick={payticket} type='button'>insert Card information</button>
-            {/* <button >Place order</button>    */}
-            <button className={ `${styles.place_order} ${showButton ? styles.show : styles.hide}`} onClick={()=> props.setCurrentStep(3)} >Place order</button>
-                </div>
-            
+              <button className={styles.creditcard_button} onClick={payticket} type="button">
+                insert Card information
+              </button>
+              {/* <button >Place order</button>    */}
+              <button className={`${styles.place_order} ${showButton ? styles.show : styles.hide}`} onClick={() => props.setCurrentStep(3)}>
+                Place order
+              </button>
+            </div>
+          </div>
         </div>
-        </div>
-        </form>
-        
-        </>
-    )
-        
-    
+      </form>
+    </>
+  );
 }
 
 export default PaymentOptions;
